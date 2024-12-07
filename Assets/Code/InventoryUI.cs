82b3d08be741b3297b83419412ada24ac32044ba
Truong5613 <143669324+Truong5613@users.noreply.github.com>
2024-12-08 00:43:49 +0700

    +2

--- Assets/Code/InventoryUI.cs
+++ Assets/Code/InventoryUI.cs
@@ -8,18 +8,20 @@ public class InventoryUI : MonoBehaviour
     public List<Canvas> ListGear;
     public List<Weapon> weapons = Item.ListWeapon;
     public List<Gear> gears = Item.ListGear;
-
+    public bool canmove = false;
     void Start()
     {
         if (Application.isMobilePlatform)
         {
-            RectTransform rectTransform = GetComponent<RectTransform>();
-            if (rectTransform != null)
-            {
-                // Adjust the position on the X-axis by 15 units
-                Vector3 newPosition = rectTransform.localPosition;
-                newPosition.x += 15;
-                rectTransform.localPosition = newPosition;
+            if (canmove) {
+                RectTransform rectTransform = GetComponent<RectTransform>();
+                if (rectTransform != null)
+                {
+                    // Adjust the position on the X-axis by 15 units
+                    Vector3 newPosition = rectTransform.localPosition;
+                    newPosition.x += 15;
+                    rectTransform.localPosition = newPosition;
+                }
             }
         }
     }
